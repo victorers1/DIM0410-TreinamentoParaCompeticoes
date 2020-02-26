d400ec92193fc887d2931ce826aa4f45728f2762
Victor Emanuel <victorers2@gmail.com>
2020-02-26 20:36:27 -0300

    :fire: using a different approach on E problem
    forget finding every permutation on string 'b', i'm now using a simplified logic to check if a letter on string 'a' has a unique corresponing on 'b'.

--- Semana1/E/E.cpp
+++ Semana1/E/E.cpp
@@ -3,75 +3,34 @@
 
 using namespace std;
 
-string a, b;
-int max_prefix = 0;
-
-int prefix(string a, string b)
-{
-    int prefix_len = 0;
-
-    for (int i = 0; i < a.size(); i++)
-    {
-        if (a[i] == b[i])
-            prefix_len++;
-        else
-            break;
-    }
-
-    return prefix_len;
-}
-
-/**
- * Função recursiva que imprime todas as permutações da string str.
- * Quando obtemos a permutação de 'b', é realizada a contagem do tamanho do prefixo comum com a string 'a'.
- * 
- */
-void permuta(string str, int l, int r)
-{
-    if (l == r) // caso base
-    {
-        int current_prefix = 0;
-
-            for (int i = 0; i < a.size(); i++)
-        {
-            if (a[i] == str[i])
-                current_prefix++;
-            else
-                break;
-        };
-
-        if (current_prefix > max_prefix)
-        {
-            max_prefix = current_prefix;
-        }
-        // cout << str << endl;
-    }
-    else
-    {
-        for (int i = l; i <= r; i++)
-        {
-            swap(str[l], str[i]);
-
-            permuta(str, l + 1, r);
-
-            swap(str[l], str[i]);
-        }
-    }
-}
-
 int main()
 {
-    int t;
+    int t, max_prefix, c;
+    string a, b;
 
     cin >> t;
 
     for (int i = 0; i < t; i++)
     {
         max_prefix = 0;
-
         cin >> a >> b;
+        for (int j = 0; j < a.size(); j++)
+        {
+            c = b.find(a[j]); //TODO: substitute for a self made function
+            // cout << "found " << a[j] << " at " << c << " in " << b << endl;
 
-        permuta(b, 0, b.size() - 1);
+            if (c > -1)
+            {
+                max_prefix++;
+                b[c] = '_';
+            }
+            else
+            {
+                break;
+            }
+
+            // cout << "b: " << b << endl;
+        }
 
         cout << max_prefix << endl;
     }
