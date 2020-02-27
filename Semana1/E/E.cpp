4b6035923b637b2591159bcb9b78a4a30c74d66b
Victor Emanuel <victorers1@hotmail.com>
2020-02-27 12:05:25 -0300

    Nova forma de resolver questão E; nova entrada para E

--- Semana1/E/E.cpp
+++ Semana1/E/E.cpp
@@ -8,6 +8,7 @@ using namespace std;
     Procura por 'c' no array e retorna seu índice, caso exista.
     Se 'c' não está contido em 's', retorna -1.
 */
+/**
 int findChar(char* s, char c)
 {
     for(int i=0; *(s+i)!='\0'; i++)
@@ -18,31 +19,68 @@ int findChar(char* s, char c)
     return -1;
 }
 
+*/
+
+void printArray(int a[], int t)
+{
+    for(int i=0; i<t; i++)
+        cout<<a[i]<<" ";
+}
+
+void zeraArray(int a[], int t)
+{
+    for(int i=0; i<t; i++)
+        a[i] = 0;
+}
+
 int main()
 {
-    int t, max_prefix, c;
+    int t, max_prefix;
     string a, b;
+
+    int dicA [26] = {0}; // dicionários com contagem de cada letra em A
+    int dicB [26] = {0}; // dicionários com contagem de cada letra em B
 
     cin >> t;
 
     for (int i = 0; i < t; i++)
     {
         max_prefix = 0;
+
+        zeraArray(dicA, 26);
+        zeraArray(dicB, 26);
+
         cin >> a >> b;
         for (int j = 0; j < a.size(); j++)
         {
-            c = findChar(&b[0], a[j]);
+            dicA[a[j]-97]++; // mapeia 'a' para 0 até 'z' para 25
+        }
 
-            if (c > -1)
-            {
+        for (int j = 0; j < b.size(); j++)
+        {
+            dicB[b[j]-97]++;
+        }
+
+        /*
+        cout<<"a: "<<endl;
+        printArray(dicA, 26);
+        cout<<endl;
+        cout<<"b: "<<endl;
+        printArray(dicB, 26);
+        cout<<endl;
+        */
+
+        for(int j = 0; j < a.size(); j++)
+        {
+            cout<<"qtd de "<<a[j]<<" em a: "<<dicA[a[j]-97]<<endl;
+            cout<<"qtd de "<<a[j]<<" em b: "<<dicB[a[j]-97]<<endl;
+
+            //max_prefix += dicA[a[j]] < dicB[a[j]] ? dicA[a[j]] : dicB[a[j]]; // soma a menor entre as quantidade de letras ao contador de prefixo
+
+            if(dicA[a[j]-97] <= dicB[a[j]-97])
                 max_prefix++;
-                b[c] = '_';
-            }
             else
-            {
                 break;
-            }
-
         }
 
         cout << max_prefix << endl;
