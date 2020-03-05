aa870557dc7f7f57a30fdfe0a75a8269e4c9933a
Victor Emanuel <victorers2@gmail.com>
2020-03-05 12:13:39 -0300

    :sparkles: wrong answer at D

--- Semana2/D/D.cpp
+++ Semana2/D/D.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
-int main() {
-       
+int main()
+{
+    int N;
+    int W, w, l;
+    int areaTotal=0;
+
+    cin >> W >> N;
+
+    for (int i = 0; i < N; i++)
+    {        
+        cin >> w >> l;
+        areaTotal += w * l;
+    }
+
+    cout << areaTotal / W << endl;
     return 0;
 }
